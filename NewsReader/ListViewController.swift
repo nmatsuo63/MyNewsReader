e91c5af27b1e921c353e0a4fe024a3280d99bba7
nmatsuo63 <nmatsuo.63@gmail.com>
2018-08-14 22:32:48 +0900

    This is the first time to push to "MyNewsReader"

---
+++ NewsReader/ListViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ListViewController.swift
+//  NewsReader
+//
+//  Created by Naoya Matsuo on 2018/08/13.
+//  Copyright © 2018年 Naoya Matsuo. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UITableViewController {
+    
+    override func tableView(_ tableView: UITableView,
+                            numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath)
+        -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Cell",
+                                                     for: indexPath)
+            return cell
+    }
+}
